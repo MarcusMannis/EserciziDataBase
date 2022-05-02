282663513f82f38eee0e1088f48e6490ccb6dc6d
MarcusMannis <m_manni@hotmail.it>
2022-05-02 16:09:13 +0200

    esercizio 3

--- EserciziAnagrafe.sql
+++ EserciziAnagrafe.sql
@@ -3,3 +3,9 @@ Esercizio 1
 select Persona.Nome, Città.Regione
 from Persona left join Città on Persona.CittàNascita = Città.nome
 where Persona.Età >=18
+
+Esercizio 3
+
+select distinct Persona.CittàNascita, Persona.Nome
+from Persona join Genia on Persona.Nome = Genia.Genitore
+Where Persona.Età >=50
