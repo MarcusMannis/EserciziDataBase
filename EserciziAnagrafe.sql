369f0461a5dc992a8f3f58a7909ab82bdf5f37c8
MarcusMannis <m_manni@hotmail.it>
2022-05-02 16:57:00 +0200

    Finito con es 2 ma guardato su figma

--- EserciziAnagrafe.sql
+++ EserciziAnagrafe.sql
@@ -4,6 +4,13 @@ select Persona.Nome, Città.Regione
 from Persona left join Città on Persona.CittàNascita = Città.nome
 where Persona.Età >=18
 
+Esercizio 2
+
+select PG.Nome
+from Genia join Persona PG on Genia.Genitore=PG.Nome
+join Persona PF on Genia.Figlio=PF.Nome
+where PG.Città = PF.Città
+
 Esercizio 3
 
 select distinct Persona.CittàNascita, Persona.Nome
