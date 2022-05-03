d55253d96747faf48dcbac09b602194322db5e1b
MarcusMannis <m_manni@hotmail.it>
2022-05-03 12:48:16 +0200

    esercizio 3

--- EserciziProgrammatore.sql
+++ EserciziProgrammatore.sql
@@ -2,8 +2,8 @@ Esercizio 1
 
 select autore.codice
 from autore join programmatore on autore.codice = programmatore.codice
-    join programmatore on programma.id = autore.id
-where programmatore.anno >= 2000 and programma.linguaggio = "Java"
+    join programma on programma.id = autore.id
+where programma.anno>=2000 and programma.linguaggio="java"
 
 Esercizio 2
 
@@ -12,3 +12,11 @@ from autore join programmatore on autore.codice = programmatore.codice
     join programma  on programma.id = autore.id
 where programma.linguaggio != "Python"
 order by programmatore.nome
+
+Esercizio 3
+
+select distinct programmatore.codice, programma.anno
+from autore join programmatore on autore.codice= programmatore.codice
+    join programma on programma.id  = autore.id
+where programmtore.categoria= 10 and programma.linguaggio != "Java"
+
