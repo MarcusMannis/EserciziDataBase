9fae371ebd906ecb814d24935175ef4706d3e8ae
MarcusMannis <m_manni@hotmail.it>
2022-05-03 10:46:44 +0200

    es 5 e 6

--- EserciziAnagrafe.sql
+++ EserciziAnagrafe.sql
@@ -6,14 +6,14 @@ where Persona.Età >=18
 
 Esercizio 2
 
-select PG.Nome
+select PF.CittàNascita as CittàNascita, Genia.Figlio, Genia.Genitore, PG.CittàNascita as CittàNascitaG
 from Genia join Persona PG on Genia.Genitore=PG.Nome
-join Persona PF on Genia.Figlio=PF.Nome
-where PG.Città = PF.Città
+    join Persona PF on Genia.Figlio=PF.Nome
+where PG.CittàNascita = PF.CittàNascita
 
 Esercizio 3
 
-select distinct Persona.CittàNascita, Persona.Nome
+select distinct Persona.CittàNascita
 from Persona join Genia on Persona.Nome = Genia.Genitore
 Where Persona.Età >=50
 
@@ -21,3 +21,16 @@ Esercizio 4
 
 select distinct F.Genitore
 from Genia F join Genia G on G.Genitore = F.Figlio
+
+Esercizio 5
+
+select count(*) as NumeroMaschiNelLazio
+from Persona join Città on Persona.CittàNascita =Città.Nome
+where sesso= "M" and Regione = "Lazio"
+
+Esercizio 6
+
+select Persona.CittàNascita, count(distinct Persona.Nome)
+from Persona join Genia on Persona.Nome = Genia.Genitore
+where Persona.sesso="F"
+group by Persona.CittàNascita
