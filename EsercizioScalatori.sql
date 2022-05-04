96950325b20463eb381d44988ee5fe00150620bc
MarcusMannis <m_manni@hotmail.it>
2022-05-04 12:14:34 +0200

    es 2

--- EsercizioScalatori.sql
+++ EsercizioScalatori.sql
@@ -2,3 +2,11 @@ Esercizio 1
 
 select s.scalatore as A, n.continente as B
 from scalata s join nazione n on s.nazione = n.nome
+
+Esercizio 2
+
+select scalatore.CF, nazione.continente
+from scalatore join scalata on scalatore.CF = scalata.scalatore
+    join nazione on nazione.nome = scalata.nazione
+where scalatore.annoNascita < 1980
+group by scalatore.CF, nazione.continente
