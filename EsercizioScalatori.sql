3ae7aa171773a6e4f0a8f1bac0c38d9bf9577395
MarcusMannis <m_manni@hotmail.it>
2022-05-04 13:05:58 +0200

    es corretti

--- EsercizioScalatori.sql
+++ EsercizioScalatori.sql
@@ -1,6 +1,6 @@
 Esercizio 1
 
-select s.scalatore as A, n.continente as B
+select distinct s.scalatore as A, n.continente as B
 from scalata s join nazione n on s.nazione = n.nome
 
 Esercizio 2
@@ -9,7 +9,7 @@ select scalatore.CF, nazione.continente
 from scalatore join scalata on scalatore.CF = scalata.scalatore
     join nazione on nazione.nome = scalata.nazione
 where scalatore.annoNascita < 1980
-group by scalatore.CF, nazione.continente
+order by scalatore.CF, nazione.continente
 
 Esercizio 3
 
