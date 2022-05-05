38635365217a14480fe63a046dd1a4e626b7a34f
MarcusMannis <m_manni@hotmail.it>
2022-05-05 10:37:41 +0200

    es 7

--- EsercizioScalatori.sql
+++ EsercizioScalatori.sql
@@ -5,7 +5,7 @@ from scalata s join nazione n on s.nazione = n.nome
 
 Esercizio 2
 
-select scalatore.CF, nazione.continente
+select distinct scalatore.CF, nazione.continente
 from scalatore join scalata on scalatore.CF = scalata.scalatore
     join nazione on nazione.nome = scalata.nazione
 where scalatore.annoNascita < 1980
@@ -20,8 +20,22 @@ where (scalata.anno - scalatore.annoNascita) <18
 
 Esercizio 4
 
-select nazione.nome, count(nazione.nome)
+select scalata.nazione, count(*)
+from scalatore join scalata on scalatore.cf = scalata.scalatore
+where scalatore.nazioneNascita = scalata.nazione
+group by scalata.nazione
+
+Esercizio 6
+use scalatori;
+select scalatore.cf, scalatore.nazioneNascita, nazione.continente, scalata.nazione
+from scalatore left join scalata on cf = scalatore 
+    join nazione on nome =nazioneNascita
+where continente != "America"
+
+Esercizio 7
+
+select scalata.anno, scalata.nazione, count(*) as numeroScsalate
 from scalatore join scalata on scalatore.cf = scalata.scalatore
     join nazione on nazione.nome = scalata.nazione
-where scalatore.nazioneNascita = scalata.nazione
-group by nazione.nome
+group by scalata.anno, scalata.nazione having count(*)>1
+order by scalata.anno
